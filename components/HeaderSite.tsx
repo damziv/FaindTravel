4236f91d6aab4218eed08d63352dc4cbecb24ad8
damziv <56076579+damziv@users.noreply.github.com>
2023-06-26 21:21:10 +0200

    Update HeaderSite.tsx

--- components/HeaderSite.tsx
+++ components/HeaderSite.tsx
@@ -1,7 +1,7 @@
 export function HeaderSite() {
   return (
     <header className="container flex items-center flex-col px-4 py-4  md:flex-row md:justify-between lg:px-0">
-      <h1 className="text-5xl font-bold">Travelio</h1>
+      <h1 className="text-5xl font-bold">Faing</h1>
     </header>
   )
 }
